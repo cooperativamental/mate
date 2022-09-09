b74d9adb39fb74a24afc4d30f99ea94150319030
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-09-09 10:19:54 -0300

    group creation in devnet

--- programs/mate/src/lib.rs
+++ programs/mate/src/lib.rs
@@ -1,15 +1,40 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("DqwzKubCtX7MkkyjPWJ1N2nuTEKoeruQkLTSoLGKMHm5");
 
 #[program]
 pub mod mate {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, name: String, ratio: u16, members: Vec<Pubkey>,) -> Result<()> {
+        let group = &mut ctx.accounts.group;
+        group.name = name;
+        group.ratio = ratio;
+        group.treasury = *ctx.accounts.treasury.key;
+        group.members = members;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = initializer,
+        space = 9000
+    )]
+    pub group: Account<'info, Group>,
+    /// CHECK:
+    pub treasury: AccountInfo<'info>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Group {
+    pub name: String,
+    pub treasury: Pubkey,
+    pub ratio: u16,
+    pub members: Vec<Pubkey>,
+}
--- tests/mate.ts
+++ tests/mate.ts
@@ -1,6 +1,7 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { Mate } from "../target/types/mate";
+import { PublicKey, SystemProgram} from "@solana/web3.js"
 
 describe("mate", () => {
   // Configure the client to use the local cluster.
@@ -9,8 +10,23 @@ describe("mate", () => {
   const program = anchor.workspace.Mate as Program<Mate>;
 
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const provider = anchor.getProvider();
+    anchor.setProvider(provider);
+  
+    const program = anchor.workspace.Mate as Program<Mate>;
+    const group = anchor.web3.Keypair.generate();
+    const treasury = anchor.web3.Keypair.generate();
+    const initializer = (program.provider as anchor.AnchorProvider).wallet;
+    
+    const tx = await program.methods.initialize("Grupo",1000,[])
+    .accounts({
+      group: group.publicKey,
+      treasury: treasury.publicKey,
+      initializer: initializer.publicKey,
+      systemProgram: SystemProgram.programId,
+    })
+    .signers([group])
+    .rpc();
     console.log("Your transaction signature", tx);
   });
 });
