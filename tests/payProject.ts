6f21ff11116f34b550d0e2f21ba437ef50bcea69
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-10-05 18:16:16 -0300

    yes

--- tests/payProject.ts
+++ tests/payProject.ts
@@ -11,13 +11,13 @@ describe("We Create a Project and then pay for it", () => {
   const payer = (program.provider as anchor.AnchorProvider).wallet;
   const member0 = (program.provider as anchor.AnchorProvider).wallet;
   const member1 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member2 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member3 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member4 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member5 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member6 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member7 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
-  const member8 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
+  const member2 = new PublicKey("CUtKCTar8gb5VYCDWbX5yFMVrhbnod9aCNf4cfhD2qPK")
+  const member3 = new PublicKey("6ivuS4xbpr61vMgMwPPnvhSw2sWs4fpKFt7baoiK3s3S")
+  const member4 = new PublicKey("8i5WmmdGgibZ36h3xLEDsQ6N8QY71RpZqUoVieYZx1GL")
+  const member5 = new PublicKey("GpMhH3hTKdGnJqAaXM9tZoq1GUSFMFNxN8bE5vZzrK6x")
+  const member6 = new PublicKey("3dwJ6Xr534orZTwa8n2SJZQNeHY2FWLJ6SpxitBz3wva")
+  const member7 = new PublicKey("5G2xXJ46kPo3R4SNmFRCg2z2ru68LCoeoWvZK3FfUGMF")
+  const member8 = new PublicKey("6R3MnM7LrMpLSs5NRnniEQ6kDxTHfpY8Qg9hXMKVkaf4")
   const member9 = new PublicKey("EjPpXXDykPawauyZHsBMtxGwG7K4iFmxdvB6ockM56ZN")
   const treasury = anchor.web3.Keypair.generate();
   const payments = [] as unknown as Mate["types"][0]
@@ -27,8 +27,8 @@ describe("We Create a Project and then pay for it", () => {
 
     console.log(payer.publicKey)
     const tx = await program.methods.createProject(
-      'will pay',
-      "eeee",
+      'Buenas Noticias',
+      "algo",
       "v0",
       1000,
       [
