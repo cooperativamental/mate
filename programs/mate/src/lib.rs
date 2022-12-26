6d30e7f4defcdd0b53feb72ff067017848073bac
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2022-12-26 12:21:22 -0300

    events

--- programs/mate/src/lib.rs
+++ programs/mate/src/lib.rs
@@ -19,11 +19,15 @@ pub mod mate {
     ) -> Result<()> {
         let group = &mut ctx.accounts.group;
         group.name = (*name).to_string();
-        group.ratio = if name == "" {0} else {ratio};
-        group.members = members;
+        group.ratio = if name == "" { 0 } else { ratio };
+        group.members = (*members).to_vec();
         group.bump = *ctx.bumps.get("group").unwrap();
 
-        emit!(GroupChanged { name });
+        emit!(GroupCreated {
+            name,
+            ratio,
+            members
+        });
 
         Ok(())
     }
@@ -35,18 +39,16 @@ pub mod mate {
         project_type: String,
         ratio: u16,
         payments: Vec<Payment>,
+        common_expenses: u64,
         currency: String,
         amount: u64,
+        milestones: u8,
         start_date: u64,
         end_date: u64,
         client: Pubkey,
     ) -> Result<()> {
         let project = &mut ctx.accounts.project;
-        project.name = (*name).to_string();
-        project.group = (*group).to_string();
-        project.project_type = if group == "" {"TEAMLESS".to_string()} else{project_type};
-        project.ratio = ratio;
-        project.members = payments
+        let members: Vec<Member> = payments
             .iter()
             .map(|payment| Member {
                 pubkey: payment.member,
@@ -54,12 +56,42 @@ pub mod mate {
                 status: "INVITED".to_string(),
             })
             .collect();
-        project.currency = currency;
-        project.status = "STARTED".to_string();
+        let status = "STARTED".to_string();
+        let p_type = if group == "" {
+            "TEAMLESS".to_string()
+        } else {
+            project_type
+        };
+        project.name = (*name).to_string();
+        project.group = (*group).to_string();
+        project.project_type = (*p_type).to_string();
+        project.ratio = ratio;
+        project.members = (*members).to_vec();
+        project.currency = (*currency).to_string();
+        project.status = (*status).to_string();
         project.amount = amount;
         project.start_date = start_date;
         project.end_date = end_date;
         project.client = client;
+        project.bump = *ctx.bumps.get("project").unwrap();
+
+        emit!(ProjectCreated {
+            name,
+            group,
+            project_type: p_type,
+            ratio,
+            members,
+            currency,
+            status,
+            amount,
+            common_expenses,
+            start_date,
+            end_date,
+            client,
+            milestones,
+        });
+
+        msg!("Project {:#?} Created!", project.name);
 
         Ok(())
     }
@@ -68,7 +100,7 @@ pub mod mate {
         let project = &ctx.accounts.project;
 
         if project.status == "REJECTED" {
-            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus))
+            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus));
         }
 
         let group = &ctx.accounts.group;
@@ -84,6 +116,7 @@ pub mod mate {
             &mut ctx.accounts.member_8,
             &mut ctx.accounts.member_9,
         ];
+        let mut payed_amount: Option<u64> = Some(0);
         ctx.accounts.project.members.iter().for_each(|payment| {
             let found = members
                 .iter()
@@ -105,12 +138,13 @@ pub mod mate {
                             ctx.accounts.payer.to_account_info().clone(),
                             member.to_account_info().clone(),
                         ],
-                    )
+                    );
+                    payed_amount = payed_amount.unwrap().checked_add(payment.amount);
                 }
-                None => Ok(()),
+                None => (),
             };
         });
-        if group.ratio > 0 && group.name != "" && project.project_type != "TEAMLESS"{
+        if group.ratio > 0 && group.name != "" && project.project_type != "TEAMLESS" {
             let amount_to_group = project.amount * ctx.accounts.group.ratio as u64 / 10000;
             msg!("Paying {:#?} Lamports to Group treasury", amount_to_group);
             invoke(
@@ -124,9 +158,11 @@ pub mod mate {
                     ctx.accounts.group.to_account_info().clone(),
                 ],
             )?;
+            payed_amount = payed_amount.unwrap().checked_add(amount_to_group);
         }
-        if project.ratio > 0 {
-            let amount_to_project = project.amount * project.ratio as u64 / 10000;
+
+        if project.amount > payed_amount.unwrap() {
+            let amount_to_project = project.amount - payed_amount.unwrap();
             msg!(
                 "Paying {:#?} Lamports to project treasury",
                 amount_to_project
@@ -146,6 +182,11 @@ pub mod mate {
         let project = &mut ctx.accounts.project;
         project.status = "PAID".to_string();
 
+        emit!(ProyectPaid {
+            name: (*project.name).to_string(),
+            status: (*project.status).to_string(),
+        });
+
         msg!("Project {:#?} Paid!", project.name);
 
         Ok(())
@@ -155,16 +196,23 @@ pub mod mate {
         let project = &mut ctx.accounts.project;
 
         if project.status != "PAID" {
-            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus))
+            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus));
         }
 
         **project.to_account_info().try_borrow_mut_lamports()? -= amount;
         **ctx.accounts.receiver.try_borrow_mut_lamports()? += amount;
 
-        msg!(
-            "{:#?} Payed from project \"{:#?}\" treasury",
+        emit!(ProyectTreasuryUsed {
+            name: (*project.name).to_string(),
             amount,
-            project.name
+            receiver: ctx.accounts.receiver.key(),
+        });
+
+        msg!(
+            "{:#?} Payed from project {:#?} treasury to {:#?}",
+            amount,
+            project.name,
+            ctx.accounts.receiver.key()
         );
 
         Ok(())
@@ -175,7 +223,7 @@ pub mod mate {
         let user = &mut ctx.accounts.user;
 
         if project.status != "STARTED" {
-            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus))
+            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus));
         }
         let members_status = project
             .members
@@ -192,14 +240,35 @@ pub mod mate {
             .collect();
         project.members = members_status;
 
+        emit!(ProyectParticipationConfirmed {
+            name: (*project.name).to_string(),
+            member: ctx.accounts.user.key(),
+        });
+
         if project
             .members
             .iter()
             .all(|member| member.status == "CONFIRMED")
         {
-            project.status = "SIGNED".to_string()
+            let status = "SIGNED".to_string();
+            project.status = (*status).to_string();
+            
+            emit!(ProyectSigned {
+                name: (*project.name).to_string(),
+                group: (*project.group).to_string(),
+                project_type: (*project.project_type).to_string(),
+                ratio: project.ratio,
+                members: (*project.members).to_vec(),
+                currency: (*project.currency).to_string(),
+                status: (*status).to_string(),
+                amount: project.amount,
+                common_expenses: project.common_expenses,
+                start_date: project.start_date,
+                end_date: project.end_date,
+                client: project.client,
+                milestones: project.milestones,
+            });
         }
-
         Ok(())
     }
 
@@ -208,7 +277,7 @@ pub mod mate {
         let user = &mut ctx.accounts.user;
 
         if project.status != "STARTED" {
-            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus))
+            return Err(error!(ErrorCode::InvalidActionForProjectCurrentStatus));
         }
         let members_status = project
             .members
@@ -230,47 +299,71 @@ pub mod mate {
             .iter()
             .any(|member| member.status == "REJECTED")
         {
-            project.status = "REJECTED".to_string()
+            project.status = "REJECTED".to_string();
+
+            emit!(ProyectRejected {
+                name: (*project.name).to_string(),
+                status: "REJECTED".to_string(),
+                causer: user.to_account_info().key()
+            });
         }
 
         Ok(())
     }
-    
+
     pub fn update_project(
         ctx: Context<CreateProject>,
         ratio: u16,
         payments: Vec<Payment>,
+        common_expenses: u64,
         currency: String,
         amount: u64,
+        milestones: u8,
         start_date: u64,
         end_date: u64,
         client: Pubkey,
     ) -> Result<()> {
         let project = &mut ctx.accounts.project;
-        
+
         if project.status != "REJECTED" {
-            return Err(error!(ErrorCode::OnlyCanUpdateRejectedProjects))
+            return Err(error!(ErrorCode::OnlyCanUpdateRejectedProjects));
         }
+        let members: Vec<Member> = payments
+        .iter()
+        .map(|payment| Member {
+            pubkey: payment.member,
+            amount: payment.amount,
+            status: "INVITED".to_string(),
+        })
+        .collect();
 
         project.ratio = ratio;
-        project.members = payments
-            .iter()
-            .map(|payment| Member {
-                pubkey: payment.member,
-                amount: payment.amount,
-                status: "INVITED".to_string(),
-            })
-            .collect();
-        project.currency = currency;
+        project.members = (*members).to_vec();
+        project.currency = (*currency).to_string();
         project.status = "STARTED".to_string();
         project.amount = amount;
         project.start_date = start_date;
         project.end_date = end_date;
         project.client = client;
 
+        emit!(ProjectUpdated {
+            name: (*project.name).to_string(),
+            group: (*project.group).to_string(),
+            project_type: (*project.project_type).to_string(),
+            ratio,
+            members,
+            currency: (*currency).to_string(),
+            status:"STARTED".to_string(),
+            amount,
+            common_expenses,
+            start_date,
+            end_date,
+            client,
+            milestones,
+        });
+
         Ok(())
     }
-
 }
 
 #[derive(Accounts)]
@@ -376,9 +469,7 @@ pub struct ConfirmProjectParticipation<'info> {
 #[derive(Accounts)]
 #[instruction(name: String, group: String)]
 pub struct UpdateProject<'info> {
-    #[account(
-        mut
-    )]
+    #[account(mut)]
     pub project: Account<'info, Project>,
     /// CHECK:
     #[account(mut)]
@@ -426,8 +517,86 @@ pub struct Member {
 }
 
 #[event]
-pub struct GroupChanged {
+pub struct GroupCreated {
     pub name: String,
+    pub ratio: u16,
+    pub members: Vec<Pubkey>,
+}
+
+#[event]
+pub struct ProjectCreated {
+    pub name: String,
+    pub group: String,
+    pub project_type: String,
+    pub ratio: u16,
+    pub members: Vec<Member>,
+    pub currency: String,
+    pub status: String,
+    pub amount: u64,
+    pub common_expenses: u64,
+    pub start_date: u64,
+    pub end_date: u64,
+    pub client: Pubkey,
+    pub milestones: u8,
+}
+
+#[event]
+pub struct ProyectPaid {
+    pub name: String,
+    pub status: String,
+}
+
+#[event]
+pub struct ProyectTreasuryUsed {
+    name: String,
+    amount: u64,
+    receiver: Pubkey,
+}
+
+#[event]
+pub struct ProyectRejected {
+    pub name: String,
+    pub status: String,
+    pub causer: Pubkey,
+}
+#[event]
+pub struct ProyectParticipationConfirmed {
+    name: String,
+    member: Pubkey,
+}
+
+#[event]
+pub struct ProyectSigned {
+    pub name: String,
+    pub group: String,
+    pub project_type: String,
+    pub ratio: u16,
+    pub members: Vec<Member>,
+    pub currency: String,
+    pub status: String,
+    pub amount: u64,
+    pub common_expenses: u64,
+    pub start_date: u64,
+    pub end_date: u64,
+    pub client: Pubkey,
+    pub milestones: u8,
+}
+
+#[event]
+pub struct ProjectUpdated {
+    pub name: String,
+    pub group: String,
+    pub project_type: String,
+    pub ratio: u16,
+    pub members: Vec<Member>,
+    pub currency: String,
+    pub status: String,
+    pub amount: u64,
+    pub common_expenses: u64,
+    pub start_date: u64,
+    pub end_date: u64,
+    pub client: Pubkey,
+    pub milestones: u8,
 }
 
 #[error_code]
